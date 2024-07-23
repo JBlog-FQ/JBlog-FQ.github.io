1a08bc6d4cdf2e319b5c248b1aaa1c3761e2ff4a
JBlog-FQ <wangqin2001710@163.com>
2024-07-23 17:27:08 +0800

    Site updated: 2024-07-23 17:27:07

--- js/icat.js
+++ js/icat.js
@@ -30,17 +30,6 @@ if(PublicSacrificeDay()){
       }
   }
 
-/*banner */
-var bywind = {
-  hideTodayCard: function() {
-      document.getElementById("todayCard") && document.getElementById("todayCard").classList.add("hide")
-  }
-}
-$(".topGroup").hover((function() {}
-), (function() {
-      document.getElementById("todayCard").classList.remove("hide"),
-      document.getElementById("todayCard").style.zIndex = 1
-  }
-))
+
 
 
