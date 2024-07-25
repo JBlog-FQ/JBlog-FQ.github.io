cdd3971f096703b34d55a8856601f838175aeed7
JBlog-FQ <wangqin2001710@163.com>
2024-07-25 14:29:41 +0800

    Site updated: 2024-07-25 14:29:40

--- bywind/random.js
+++ bywind/random.js
@@ -1,1 +1,1 @@
-var posts=["posts/7f1ae6d2/","posts/f00f6d65/","posts/69e7170f/","posts/30b33380/","posts/65682cd8/","posts/30b33380/","posts/f5b2a257/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["posts/7f1ae6d2/","posts/f00f6d65/","posts/69e7170f/","posts/30b33380/","posts/65682cd8/","posts/f5b2a257/","posts/30b33380/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
